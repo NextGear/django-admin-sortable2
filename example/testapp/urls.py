2a5be6298147a9af769405310f9d84201f28d8a8
Vincent Spanjersberg <vincent@nextgear.nl>
2017-11-23 09:45:18 +0100

    tmp changes for django 2.0

--- example/testapp/urls.py
+++ example/testapp/urls.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
